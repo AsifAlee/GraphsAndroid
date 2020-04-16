0538e3cf66bc3c83ca6cd58c50158c4307c66267
AsifAlee <51690337+AsifAlee@users.noreply.github.com>
2020-04-16 15:24:12 +0500

    Initial commit

---
+++ app/src/main/java/com/example/graphsandroid/CartesianPlotter.java
@@ -0,0 +1,22 @@
+package com.example.graphsandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public class CartesianPlotter extends AppCompatActivity  {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cartesian_plotter);
+        SurfaceView surfaceView = findViewById(R.id.surfaceView);
+
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/graphsandroid/MainActivity.java
@@ -0,0 +1,138 @@
+package com.example.graphsandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Toast;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.Viewport;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.DataPointInterface;
+import com.jjoe64.graphview.series.LineGraphSeries;
+import com.jjoe64.graphview.series.OnDataPointTapListener;
+import com.jjoe64.graphview.series.Series;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    GraphView graph;
+    Canvas canvas;
+    Paint paint;
+    ArrayList<DataPoint> dataPoints;
+    ArrayList<Point> canvasPoints;
+    private static final String TAG = "MainActivity";
+    int counter = 0;
+    LineGraphSeries<DataPoint> series;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        canvas = new Canvas();
+        dataPoints = new ArrayList<>();
+        canvasPoints = new ArrayList<>();
+        paint = new Paint();
+        paint.setStrokeWidth(4f);
+        //  paint.setColor(Color.BLACK);
+        graph = findViewById(R.id.graphView);
+        Viewport viewport = graph.getViewport();
+        graph.getViewport().setXAxisBoundsManual(true);
+        graph.getViewport().setYAxisBoundsManual(true);
+        graph.getViewport().setScrollable(true);
+        graph.getViewport().setScalable(true);
+        viewport.setMaxX(150);
+        viewport.setMinX(-150);
+        viewport.setMaxY(150);
+        viewport.setMinY(0);
+
+        series = new LineGraphSeries<>();
+        for (int x = -150; x <= 150; x++) {
+            for (int y = 0; y <= 150; y++) {
+                DataPoint dataPoint = new DataPoint(x, y);
+                series.appendData(dataPoint, false, 100000);
+            }
+        }
+        graph.addSeries(series);
+
+        series.setOnDataPointTapListener(new OnDataPointTapListener() {
+            @Override
+            public void onTap(Series series, DataPointInterface dataPoint) {
+                Toast.makeText(MainActivity.this, "Datapoints are " + dataPoint.getX()
+                        + " " + dataPoint.getY(), Toast.LENGTH_LONG).show();
+
+                DataPoint dataPoint1 = (DataPoint) dataPoint;
+                int x = (int) dataPoint1.getX();
+                int y = (int) dataPoint1.getY();
+                dataPoints.add(dataPoint1);
+                canvasPoints.add(new Point(x, y));
+                Log.d(TAG, "onTap: " + dataPoints.size());
+
+                for (int i = 0; i < canvasPoints.size(); i++) {
+                    Point point = canvasPoints.get(i);
+                    canvas.drawCircle(point.x, point.y, 10, paint);
+
+
+                    // Draw line with next point (if it exists)
+                    if (i + 1 < canvasPoints.size()) {
+                        Point next = canvasPoints.get(i + 1);
+                        canvas.drawLine(point.x, point.y, next.x, next.y, paint);
+                    }
+                }
+            }
+        });
+    }
+
+    private class DemoView extends View {
+        public DemoView(Context context){
+            super(context);
+        }
+
+        @Override protected void onDraw(final Canvas canvas) {
+            super.onDraw(canvas);
+
+            series.setOnDataPointTapListener(new OnDataPointTapListener() {
+                @Override
+                public void onTap(Series series, DataPointInterface dataPoint) {
+                    Toast.makeText(MainActivity.this,"Datapoints are "+dataPoint.getX()
+                            +" "+dataPoint.getY(),Toast.LENGTH_LONG).show();
+
+                    DataPoint dataPoint1 = (DataPoint) dataPoint;
+                    int x = (int) dataPoint1.getX();
+                    int y = (int) dataPoint1.getY();
+                    dataPoints.add(dataPoint1);
+                    canvasPoints.add(new Point(x,y));
+                    Log.d(TAG, "onTap: "+dataPoints.size());
+
+                    for(int i = 0;i < canvasPoints.size();i++){
+                        final Canvas canvas1 = canvas;
+                        Point point = canvasPoints.get(i);
+                        canvas1.drawCircle(point.x,point.y,10,paint);
+
+
+                        // Draw line with next point (if it exists)
+                        if (i + 1 < canvasPoints.size()) {
+                            Point next = canvasPoints.get(i + 1);
+                            canvas.drawLine(point.x, point.y, next.x, next.y, paint);
+                        }
+                    }
+
+
+
+                }
+            });
+        }
+    }
+
+
+}
